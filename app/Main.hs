0937462092ca347659c8816f9863395fd3280f02
Dylan Tang <dtang04@uchicago.edu>
2026-01-28 01:17:14 -0600

    Comment still wrong, change tmrw

--- app/Main.hs
+++ app/Main.hs
@@ -16,7 +16,19 @@ splitWhen cond l = go [] [] cond l
             | cond l == True = go (current_lst ++ [current_chunk]) [] cond ls
             | otherwise      = go current_lst (current_chunk ++ [l]) cond ls
 
+scanl' :: (b -> a -> b) -> b -> [a] -> [b]
+scanl' _ z [] = [z]
+scanl' f z lst = reverse (foldl build [z] lst)
+    where
+        build (b:bs) x = f b x : b : bs
+-- What is the accumulator here?
+-- The accumulator is the list of results we have built so far, starting with [z].
+-- How do we know what build is?
+-- Consider scanl' (+) 0 [1,2,3] -> build (build(build [0] 1) 2) 3
+--                               -> build [0] 1 must be [0,1]
+--                               -> build [0,1] 2 must be [0,1,3]x
 main :: IO ()
 main = do
     print (dedup [1,1,2,2,3,3,3,4,4,5])
     print (splitWhen (== ',') "a,b,")
+    print (scanl' (+) 0 [1,2,3,4])
